cf305c934b5bf851253f147c91b14d296193f9a7
Nick Erokhin <nickerokhin@gmail.com>
2018-01-19 12:31:48 -0700

    Padding image with replicate on both ends, simplified convolution

--- HW1/meanFilter.m
+++ HW1/meanFilter.m
@@ -4,40 +4,19 @@ function [ outImg ] = meanFilter(inImg, kernel_size)
     rgbArr = imSize(3);
     xSize = imSize(2);
     ySize = imSize(1);
-    kernel_overlap = floor(kernel_size/2);
+    k_overlap = floor(kernel_size/2);
+    inImg = padarray(inImg, [k_overlap k_overlap], 'replicate', 'both');
     for rgb = 1:rgbArr
         %Loop through rgb pixels
-        for col = 1:xSize
+        for col = 1 + k_overlap:xSize - k_overlap
             %loop through rows
-            for row = 1:ySize
-                colBuff = -floor(kernel_size/2);
-                rowBuff = -floor(kernel_size/2);
-                convSum = 0;
-                kernShadow = zeros(3,3);
-                for colKern = 1:kernel_size
-                    rowBuff = -floor(kernel_size/2);
-                    for rowKern = 1:kernel_size
-                        modRow = mod(row + rowBuff, ySize);
-                        
-                        if modRow == 0
-                            modRow = 1;
-                        end
-                        
-                        modCol = mod(col + colBuff, xSize);
-                        
-                        if modCol == 0
-                            modCol = 1;
-                        end
-                     
-                        convSum = convSum + kernel(rowKern,colKern) * inImg(modRow, modCol, rgb);
-                        rowBuff = rowBuff + 1;
-                        
-                        outImg(row,col,rgb) = convSum;
-                    end
-                    colBuff = colBuff + 1;
-                end     
+            for row = 1 + k_overlap:ySize - k_overlap
+               kernShadow = inImg(row - k_overlap:row + k_overlap, col - k_overlap:col + k_overlap);
+               outImg(row - k_overlap, col - k_overlap, rgb) = mean2(kernShadow)
             end
+            
         end
     end
+    size(outImg)
 end
     

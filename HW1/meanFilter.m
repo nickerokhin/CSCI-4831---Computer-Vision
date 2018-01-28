f3715754036b9146ca22c1d2bf0a1dc5b348e07b
Nick Erokhin <nickerokhin@gmail.com>
2018-01-27 18:48:15 -0700

    Works for any kernel size, kernel size adjusts on edges and corners

--- HW1/meanFilter.m
+++ HW1/meanFilter.m
@@ -1,19 +1,54 @@
 function [ outImg ] = meanFilter(inImg, kernel_size)
-    kernel = ones(kernel_size, kernel_size) * (1/(kernel_size*kernel_size));
-    k_overlap = floor(kernel_size/2);
-    inImg = padarray(inImg, [k_overlap k_overlap], 'replicate', 'both');
+
     imSize = size(inImg);
+    outImg = zeros(imSize);
     rgbArr = imSize(3);
     xSize = imSize(2);
     ySize = imSize(1);
-    size(inImg)
+
+%location of source pixel in relation to kernel frame
+
+    kCenterCol = ceil(kernel_size/2);
+    kCenterRow = ceil(kernel_size/2);
+
+    %Calculating distances due to n,m potentially being even or odd
+
+    kVertEdgeDistTop = kCenterRow - 1;
+    kVertEdgeDistBot = kernel_size - kCenterRow;
+    kHorizEdgeDistLeft = kCenterCol - 1;
+    kHorizEdgeDistRight = kernel_size - kCenterCol;
+
+    
     for rgb = 1:rgbArr
         %Loop through rgb pixels
-        for col = 1 + k_overlap:xSize - k_overlap
+        for col = 1:xSize
             %loop through rows
-            for row = 1 + k_overlap:ySize - k_overlap
-               kernShadow = inImg(row - k_overlap:row + k_overlap, col - k_overlap:col + k_overlap, rgb);
-               outImg(row-k_overlap, col-k_overlap, rgb) = mean2(kernShadow);
+            for row = 1:ySize
+                rowStart = row - kVertEdgeDistTop;
+                rowEnd = row + kVertEdgeDistBot;
+                colStart = col - kHorizEdgeDistLeft;
+                colEnd = col + kHorizEdgeDistRight;
+         
+            %Conditionals for edges and corners
+            
+                if rowStart <= 0
+                    rowStart = 1;
+                end
+
+                if rowEnd > ySize
+                    rowEnd = ySize;
+                end
+
+                if colStart <= 0
+                    colStart = 1;
+                end
+
+                if colEnd > xSize
+                    colEnd = xSize;
+                end
+
+                kernShadow = inImg(rowStart:rowEnd, colStart:colEnd, rgb);
+                outImg(row, col, rgb) = mean2(kernShadow);
             end
             
         end

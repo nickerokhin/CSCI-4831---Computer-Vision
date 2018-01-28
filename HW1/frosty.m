5237e785f78c617929226fe0f330baa8dd630037
Nick Erokhin <nickerokhin@gmail.com>
2018-01-27 19:21:23 -0700

    Clean up and comments

--- HW1/frosty.m
+++ HW1/frosty.m
@@ -1,11 +1,20 @@
 function [ outImg ] = frosty( inImg, n, m)
 
+%Dimensions of image
+
 imSize = size(inImg);
+outImg = zeros(imSize);
 rgbArr = imSize(3);
 xSize = imSize(2);
 ySize = imSize(1);
+
+%location of source pixel in relation to kernel frame
+
 kCenterCol = ceil(m/2);
 kCenterRow = ceil(n/2);
+
+%Calculating distances due to n,m potentially being even or odd
+
 kVertEdgeDistTop = kCenterRow - 1;
 kVertEdgeDistBot = n - kCenterRow;
 kHorizEdgeDistLeft = kCenterCol - 1;
@@ -17,51 +26,37 @@ for rgb = 1:rgbArr
     for col = 1:xSize
         for row = 1:ySize
             
+            %Bounds for kernel range on source image using distance
+            
             rowStart = row - kVertEdgeDistTop;
             rowEnd = row + kVertEdgeDistBot;
             colStart = col - kHorizEdgeDistLeft;
             colEnd = col + kHorizEdgeDistRight;
+         
+            %Conditionals for edges and corners
             
-            %{
-            %Top left corner
-            if and(row - kVertEdgeDistTop <= 0, col - kHorizEdgeDistLeft <= 0)
-                rowStart = 1
-                colStart = 1
-           
-            %Top right corner
-            elseif and(row - kVertEdgeDistTop <= 0, col + kHorizEdgeDistRight > xSize)
-                rowStart = 1
-                colEnd = xSize
-                
-                
-            %Bottom left corner
-            elseif 
-            %}
-            
-            %Bottom right corner
-            %Top row
-            %Leftmost col
-            %Rightmost col
-            %Bottom row
-            
-            
-            if row - kVertEdgeDistTop <= 0
+            if rowStart <= 0
                 rowStart = 1;
             end
             
-            if row + kVertEdgeDistTop > ySize
+            if rowEnd > ySize
                 rowEnd = ySize;
             end
             
-            if col - kHorizEdgeDistLeft <= 0
+            if colStart <= 0
                 colStart = 1;
             end
                 
-            if col + kHorizEdgeDistRight > xSize
+            if colEnd > xSize
                 colEnd = xSize;
             end
             
+
+            
             inImgSample = inImg(rowStart:rowEnd, colStart:colEnd, rgb);
+            
+            %Selects random element from random row in inImgSample
+            
             outImg(row, col, rgb) = datasample(datasample(inImgSample, 1), 1);
             
 

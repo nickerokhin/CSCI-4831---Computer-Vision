3f5d3e2b1c8971c8884870f3dbeffc1a20f5ad29
Nick Erokhin <nickerokhin@gmail.com>
2018-01-30 21:38:32 -0700

    No longer mirrors, swirl is a bit odd

--- HW1/swirlFilter.m
+++ HW1/swirlFilter.m
@@ -1,62 +1,58 @@
 function [ outImg ] = swirlFilter( inImg, factor, ox, oy)
-
+ 
 imSize = size(inImg);
 rgbArr = imSize(3);
 xSize = imSize(2);
 ySize = imSize(1);
    
     for col = 1:xSize
-        
+       
         for row = 1:ySize
-            
-            x = col - ox;
-            y = row - oy;
-            
-            if y == 0
-                y = 1;
-            end
-            
-            if x == 0
-                x = 1;
-            end
-            
-            theta = atan(y/x);
-            radius = (x^2 + y^2)^.5;
-            scaleTheta = mod(theta + radius/factor, 2*pi)
-            x = radius * cos(scaleTheta);
-            y = radius * sin(scaleTheta);
-            [sx sy] = sampleNearest(x,y, imSize, ox, oy);
-            
            
-            outImg(row,col, :) = inImg(sx,sy, :);
-            
-            
+            x = col;
+            y = row;
+           
+           
+            radx = ((x^2 + y^2)^.5);
+            theta = atan(y/x);
+            if isnan(theta)
+                theta = 0
+            end
+            radius = ((ox-x)^2 + (oy-y)^2)^.5;
+            scaleTheta = theta + (radius/100)*factor;
+            x = radx * cos(scaleTheta);
+            y = radx * sin(scaleTheta);
+            [sx sy] = sampleNearest(x,y, imSize, ox, oy);
+           
+           
+            outImg(row,col, :) = inImg(sy,sx, :);
+           
+           
         end
-        
+       
     end
-    
+   
 end
-
+ 
 function [ newX, newY ] = sampleNearest(x, y, imSize, ox, oy)
-
-newX = ceil(x) + ox;
-newY = ceil(y) + oy;
-
+ 
+newX = ceil(x);
+newY = ceil(y);
+ 
 if newX > imSize(2)
     newX = imSize(2);
 end
-
+ 
 if newY > imSize(1)
     newY = imSize(1);
 end
-
+ 
 if newY < 1
     newY = 1;
-    
+   
 end
-
+ 
 if newX < 1
     newX = 1;
 end
 end
-

1f940031182d9da6f217ba362dcfc38dd0cafde7
Nick Erokhin <nickerokhin@gmail.com>
2018-02-16 17:09:54 -0700

    properly assigns coordinates to [row col] format

--- HW2/getPoints.m
+++ HW2/getPoints.m
@@ -3,6 +3,7 @@
 %HW 1
 %Ioana Fleming
 
+%tower2, tower1
 function [ outPoints ] = getPoints( inImg1, inImg2 )
 
 figure(1);
@@ -18,9 +19,9 @@ title('Image2');
 for i = 1:10
     
 axes(axis1);
-outPoints(i, 1:2) = ginput(1);
+outPoints(i, 1:2) = fliplr(ginput(1));
 axes(axis2);
-outPoints(i, 3:4) = ginput(1);
+outPoints(i, 3:4) = fliplr(ginput(1));
 
 end
 
